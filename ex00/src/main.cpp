8bf41e5db7e105ffa5ca4e9ee5fee424fe3ecfcb
Shun Usami <usatie@gmail.com>
2023-04-12 12:22:23 +0900

    :sparkles: [ex00] Implement swap, min and max

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -1,6 +1,8 @@
 #include <iomanip>
 #include <iostream>
 
+#include "whatever.hpp"
+
 #define RESET "\033[m"
 #define GREEN "\033[0;32m"
 #define RED "\033[0;31m"
@@ -36,9 +38,136 @@ void printSubtitle(std::string const& subtitle) {
 
 void test_from_subject() {
   printTitle("Test from sunject pdf");
+  int a = 2;
+  int b = 3;
+  ::swap(a, b);
+  std::cout << "a = " << a << ", b = " << b << std::endl;
+  std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  std::string c = "chaine1";
+  std::string d = "chaine2";
+  ::swap(c, d);
+  std::cout << "c = " << c << ", d = " << d << std::endl;
+  std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+  std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+}
+
+void test_swap() {
+  printTitle("Test swap");
+  // Test int
+  {
+    printSubtitle("int");
+    int a = 2;
+    int b = 3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+  }
+  // Test float
+  {
+    printSubtitle("float");
+    float a = 2.2;
+    float b = 3.3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+  }
+  // Test char
+  {
+    printSubtitle("char");
+    char a = 'a';
+    char b = 'b';
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+  }
+  // Test string
+  {
+    printSubtitle("string");
+    std::string a = "string1";
+    std::string b = "string2";
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+  }
+}
+
+void test_min() {
+  printTitle("Test min");
+  // Test int
+  {
+    printSubtitle("int");
+    int a = 2;
+    int b = 3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  }
+  // Test float
+  {
+    printSubtitle("float");
+    float a = 2.2;
+    float b = 3.3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  }
+  // Test char
+  {
+    printSubtitle("char");
+    char a = 'a';
+    char b = 'b';
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  }
+  // Test string
+  {
+    printSubtitle("string");
+    std::string a = "string1";
+    std::string b = "string2";
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+  }
+}
+
+void test_max() {
+  printTitle("Test max");
+  // Test int
+  {
+    printSubtitle("int");
+    int a = 2;
+    int b = 3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  }
+  // Test float
+  {
+    printSubtitle("float");
+    float a = 2.2;
+    float b = 3.3;
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  }
+  // Test char
+  {
+    printSubtitle("char");
+    char a = 'a';
+    char b = 'b';
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  }
+  // Test string
+  {
+    printSubtitle("string");
+    std::string a = "string1";
+    std::string b = "string2";
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+  }
 }
 
 int main(void) {
+  test_swap();
+  test_min();
+  test_max();
   test_from_subject();
   return 0;
 }
